bcab03c74891f48a5c5ba6e19108f50f743fdded
TheGreatKamus <emiliofloresga@gmail.com>
2024-11-16 16:39:42 -0500

    Tridimensionales.java

--- IdeaProjects/areas_figuras/src/Bidimensionales.java
+++ IdeaProjects/areas_figuras/src/Bidimensionales.java
@@ -1,3 +1,5 @@
 public class Bidimensionales {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello world!jhiuguy");
+    }
 }
---
+++ IdeaProjects/areas_figuras/src/Tridimensionales.java
@@ -0,0 +1,5 @@
+public class Tridimensionales {
+    public static void main(String[] args) {
+        System.out.println("bolivia no tiene mares");
+    }
+}
