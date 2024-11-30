aa4f7057cd5103b663234dff8058a338bcfedc5e
Jaimewww <jaimelandazuri719@gmail.com>
2024-11-30 13:14:36 -0500

    Fixxed validador/casteos

--- IdeaProjects/areas_figuras/src/controller/util/validador.java
+++ IdeaProjects/areas_figuras/src/controller/util/validador.java
@@ -1,40 +1,38 @@
 package controller.util;
-public class validador{
-    public static boolean validarInt(String numero){
+public class validador {
+    public static boolean validarInt(String numero) {
         boolean intisValid;
         int numeroLenght = numero.length();
         int i = 0;
-        do{
+        do {
             char c = numero.charAt(i);
-            if(Character.isDigit(c)){
+            if (Character.isDigit(c)) {
                 intisValid = true;
-            }else{
+            } else {
                 intisValid = false;
                 break;
             }
             i++;
-        } while(i<numeroLenght);
+        } while (i < numeroLenght);
         return intisValid;
     }
-    public static boolean validarDouble(String numero){
-        boolean doubleisValid;
+
+    public static boolean validarDouble(String numero) {
+        boolean doubleisValid = false;
         int numeroLenght = numero.length();
         int i = 0, dotCount = 0;
-        do{
+        do {
             char c = numero.charAt(i);
-            if(Character.isDigit(c) || numero.charAt(i) == '.' && dotCount <= 1){
+            if (Character.isDigit(c)) {
                 doubleisValid = true;
+            } else if(numero.charAt(i) == '.' && dotCount < 1) {
                 dotCount++;
-            }else{
+            } else{
                 doubleisValid = false;
                 break;
             }
             i++;
-        } while(i<numeroLenght);
+        } while (i < numeroLenght);
         return doubleisValid;
     }
-    //public static void ejecutar(){
-    //    String numero = "..3554";
-    //    System.out.println(validador.validarInt(numero));
-    //}
 }
