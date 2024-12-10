9e6084b4f158a807a94799592af54749cd265eee
TheGreatKamus <emiliofloresga@gmail.com>
2024-12-10 17:24:36 -0500

    Poniendo dodecaedro

--- IdeaProjects/areas_figuras/src/controller/F_bidimensionales.java
+++ IdeaProjects/areas_figuras/src/controller/F_bidimensionales.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.util.Scanner;
 
+import controller.util.Utilidades;
+
 public class F_bidimensionales {
     static Scanner sc = new Scanner(System.in); 
     // 1 - 16 Sigcho
@@ -9,17 +11,19 @@ public class F_bidimensionales {
 
     // dodecaedro area 17
     public static void Ejecutar_dodecaedro() {
+        System.out.println("Ingrese el valor de la arista: ");
         double a = sc.nextDouble();
         double area = 3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(a, 2);
 
-        System.out.println("El area del dodecaedro de arista a = " + a + " es: " + area);
+        System.out.println("El area del dodecaedro de arista a = " + a + " es: " + Utilidades.Redondear_double(area));
     }
 
     // dedecaedro volumne 18
     public static void Ejecutar_dodecaedro_volumen() {
+        System.out.println("Ingrese el valor de la arista: ");
         double a = sc.nextDouble();
         double area = 3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(a, 2);
 
-        System.out.println("El area del dodecaedro de arista a = " + a + " es: " + area);
+        System.out.println("El area del dodecaedro de arista a = " + a + " es: " + Utilidades.Redondear_double(area));
     }
 }
