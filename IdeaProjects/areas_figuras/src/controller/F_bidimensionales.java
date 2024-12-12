e301445f733c900ed3e01a0430325fbcb0258a99
Jaimewww <jaimelandazuri719@gmail.com>
2024-12-12 13:09:41 -0500

    Cambios

--- IdeaProjects/areas_figuras/src/controller/F_bidimensionales.java
+++ IdeaProjects/areas_figuras/src/controller/F_bidimensionales.java
@@ -2,48 +2,41 @@ package controller;
 
 import java.util.Scanner;
 
+import controller.util.Menu;
 import controller.util.Utilidades;
 
 public class F_bidimensionales {
+    static Menu menu = new Menu();
     static Scanner sc = new Scanner(System.in);
     // 1 - 16 Sigcho
     // aca deberia ir lo se sigcho
 
     // dodecaedro area 17
     public static void Ejecutar_dodecaedro() {
-        System.out.println("Ingrese el valor de la arista: ");
-        double a = sc.nextDouble();
-        double area = 3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(a, 2);
-
+        float a = menu.readFloat("Ingrese el valor de la arista en cm: ");
+        float area = (float) (3 * Math.sqrt(25 + 10 * Math.sqrt(5)) * Math.pow(a, 2));
         System.out.println("El area del dodecaedro de arista a = " + a + " es: " + Utilidades.Redondear_double(area));
     }
 
     // dodecaedro volumne 18
     public static void Ejecutar_dodecaedro_volumen() {
-        System.out.println("Ingrese el valor de la arista: ");
-        double a = sc.nextDouble();
-        double volumen = ((15 + 7 * Math.sqrt(5)) * Math.pow(a, 3)) / 4;
-
-        System.out.println(
-                "El volumen del dodecaedro de arista a = " + a + " es: " + Utilidades.Redondear_double(volumen));
+        float a = menu.readFloat("Ingrese el valor de la arista en cm: ");
+        float volumen = (float) (((15 + 7 * Math.sqrt(5)) * Math.pow(a, 3)) / 4);
+        System.out.println("El volumen del dodecaedro de arista a = " + a + " es: " + Utilidades.Redondear_double(volumen));
     }
 
     // dodecagono 19
     public static void Ejecutar_dodecagono() {
-        System.out.println("Ingrese el valor de un lado: ");
-        double l = sc.nextDouble();
-        double area = 5 * l * (l / (2 * Math.tan(Math.toRadians(18))));
-
+        float l = menu.readFloat("Ingrese el valor de un lado en cm: ");
+        float area = (float) (5 * l * (l / (2 * Math.tan(Math.toRadians(18)))));
         System.out.println("El area del dodecaedro de lado " + l + " es: " + Utilidades.Redondear_double(area));
     }
 
     // elipse 20
     public static void Ejecutar_elipse() {
-        System.out.println("Ingrese el eje superior: ");
-        double a = sc.nextDouble();
-        System.out.println("Ingrese el eje inferior: ");
-        double b = sc.nextDouble();
-        double area = Math.PI * a * b;
+        float a = menu.readFloat("Ingrese el eje superior en cm: ");
+        float b = menu.readFloat("Ingrese el eje inferior: ");
+        float area = (float) (Math.PI * a * b);
 
         System.out.println("El area de la elipse de eje superior " + a + " y de eje inferior " +b + " es: "+ Utilidades.Redondear_double(area));
     }
